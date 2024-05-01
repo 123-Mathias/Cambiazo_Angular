f5cd82ce1b01cdddbddf6c1396d74da13b7d16c5
Mathias0112546 <113148252+123-Mathias@users.noreply.github.com>
2024-05-01 11:58:01 -0500

    V5

--- src/app/ongs/components/search-ongs/search-ongs.component.ts
+++ src/app/ongs/components/search-ongs/search-ongs.component.ts
@@ -3,10 +3,11 @@ import {MatFormField} from "@angular/material/form-field";
 import {MatIcon} from "@angular/material/icon";
 import {MatInputModule} from "@angular/material/input";
 import {MatCardModule} from "@angular/material/card";
-import {NgForOf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
 import {MatButton} from "@angular/material/button";
 import {OngsService} from "../../services/ongs.service";
 import {Ongs} from "../../models/ongs/ongs.model";
+
 @Component({
   selector: 'app-search-ongs',
   standalone: true,
@@ -16,24 +17,34 @@ import {Ongs} from "../../models/ongs/ongs.model";
     MatInputModule,
     MatCardModule,
     NgForOf,
-    MatButton
+    MatButton,
+    NgIf
   ],
   templateUrl: './search-ongs.component.html',
   styleUrl: './search-ongs.component.css'
 })
 export class SearchOngsComponent implements OnInit{
-
+  ongsSearched: string = ""
   items:Ongs[]=[]
 
   constructor(private ongsService:OngsService) {
   }
   ngOnInit() {
 
-    this.ongsService.getOngs().subscribe(
-      (res:any)=> {
-        this.items = res
-      },error => console.log(error)
-    )
+    this.ongsService.getOngs().subscribe((res:any)=>
+    { this.items = res},error => console.log(error))
   }
+
+  searchOngs(event:Event){
+    this.ongsSearched = (event.target as HTMLInputElement).value.trim()
+    this.ongsService.getOngs().subscribe((res:any)=>
+    { this.items = res.filter((ongs:Ongs)=>{
+      const ongsNameNormalized = ongs.name.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
+      return ongsNameNormalized.includes(this.ongsSearched) || this.ongsSearched == '';
+
+    })},error => console.log(error))
+
+  }
+
 
 }
