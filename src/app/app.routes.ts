e56aad4ce18e2fa5fa32a20e49ffc7b8e0d39427
Mathias0112546 <113148252+123-Mathias@users.noreply.github.com>
2024-05-02 04:41:42 -0500

    V6

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,7 +10,18 @@ import {PostComponent} from "./pages/post/post.component";
 import {LoginComponent} from "./pages/login/login.component";
 import {RegisterComponent} from "./pages/register/register.component";
 
+import {HomeAdminComponent} from "./admin/pages/home-admin/home-admin.component";
+import {OngsCategoriesAdminComponent} from "./admin/pages/ongs-categories-admin/ongs-categories-admin.component";
+import {OngsAdminComponent} from "./admin/pages/ongs-admin/ongs-admin.component";
+import {
+  ProductsCategoriesAdminComponent
+} from "./admin/pages/products-categories-admin/products-categories-admin.component";
+import {ProductsAdminComponent} from "./admin/pages/products-admin/products-admin.component";
+import {MembershipsAdminComponent} from "./admin/pages/memberships-admin/memberships-admin.component";
+import {UsersAdminComponent} from "./admin/pages/users-admin/users-admin.component";
+
 export const routes: Routes = [
+
   {path:'home',component: HomeComponent},
   {path:'privacy-policy', component: PrivacyPolicyComponent},
   {path:'help', component: HelpComponent},
@@ -21,6 +32,15 @@ export const routes: Routes = [
   {path:'login',component: LoginComponent },
   {path:'register',component: RegisterComponent },
   {path:'home/post',component:PostComponent},
+
+  /**************   Admin    *********************/
+  {path: 'admin', component: HomeAdminComponent},
+  {path: 'admin/ongs-categories', component: OngsCategoriesAdminComponent},
+  {path: 'admin/ongs', component: OngsAdminComponent},
+  {path: 'admin/products-categories', component: ProductsCategoriesAdminComponent},
+  {path: 'admin/products', component: ProductsAdminComponent},
+  {path: 'admin/memberships', component: MembershipsAdminComponent},
+  {path: 'admin/users', component: UsersAdminComponent},
 
   {path:'',pathMatch:'full',redirectTo:'home'},
   {path:'**',pathMatch:'full',redirectTo:'home'}
